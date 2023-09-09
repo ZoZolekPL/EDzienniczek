ee3dc169ed4fc1efab6f6e73ae930fa43da0194d
Szymon <107434156+ZoZolekPL@users.noreply.github.com>
2023-09-09 18:36:58 +0200

    first

---
+++ src/test/java/com/example/E_dzienniczek/EDzienniczekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.E_dzienniczek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EDzienniczekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
